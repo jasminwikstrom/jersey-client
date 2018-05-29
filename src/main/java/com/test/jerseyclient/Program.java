47942f3923fd99a1c8eea4a462ae81fcf34560ec
jasminwikstrom <32257481+jasminwikstrom@users.noreply.github.com>
2018-05-29 19:33:41 +0200

    Added 2 tests

--- src/main/java/com/test/jerseyclient/Program.java
+++ src/main/java/com/test/jerseyclient/Program.java
@@ -1,14 +1,12 @@
 package com.test.jerseyclient;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationConfig;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.client.*;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
@@ -24,8 +22,16 @@ public class Program implements CommandLineRunner {
     public static void main(String[] args) throws IOException {
         System.out.println("Starting program");
 
+        System.out.println("*****************USERS*****************");
         testGetAllUsers();
+        System.out.println("*****************TEAMS*****************");
+        testGetAllTeams();
+        System.out.println("*****************WORKITEMS*****************");
+        testGetAllWorkItems();
+        System.out.println("*****************Added USER*****************");
+        testAddUser();
     }
+
 
     private static void testGetAllUsers() throws IOException {
         Client client = ClientBuilder.newClient();
@@ -43,4 +49,56 @@ public class Program implements CommandLineRunner {
 
         System.out.println(indented);
     }
+
+    private static void testGetAllTeams() throws IOException {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080").path("teams");
+
+
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
+                .get();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        Object json = mapper.readValue(response.readEntity(String.class), Object.class);
+        String indented = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(json);
+
+        System.out.println(indented);
+    }
+
+
+    private static void testGetAllWorkItems() throws IOException {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target("http://localhost:8080").path("workitems");
+
+
+        Response response = target.request(MediaType.APPLICATION_JSON_TYPE)
+                .get();
+
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        Object json = mapper.readValue(response.readEntity(String.class), Object.class);
+        String indented = mapper.writerWithDefaultPrettyPrinter()
+                .writeValueAsString(json);
+
+        System.out.println(indented);
+    }
+
+
+    private static void testAddUser() throws IOException {
+
+        Client client = ClientBuilder.newClient();
+        Invocation.Builder builder = client
+                .target("http://localhost:8080/users")
+                .request(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Jasse");
+
+        String input = "{\"firstName\":\"Ett namn\",\"lastName\":\"Ett Efternamn\",\"username\":\"Ett username\",\"status\":\"true\"}";
+
+        Response response = builder.post(Entity.json(input));
+        System.out.println(response.getHeaders());
+
+    }
+
 }
